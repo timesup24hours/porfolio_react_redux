18cc76bef95adf222a033012185863b1c0f0ea72
henry <henry@mpb.local>
2017-04-13 21:20:32 -0700

    require user to login to write a review, hide the review form when user is not login

--- frontend/src/components/product/ReviewForm.js
+++ frontend/src/components/product/ReviewForm.js
@@ -6,6 +6,12 @@ const ReviewForm = props => {
 
   const handleSubmitComment = e => {
     e.preventDefault()
+
+    if(!props.auth.isAuthenticated) {
+      document.querySelector('.ReviewForm-error').innerHTML = 'login is required'
+      return false
+    }
+
     const review = document.querySelector('#ReviewForm-textarea').value
 
     props.submitReviewRequest({
@@ -19,35 +25,38 @@ const ReviewForm = props => {
 
   return (
     <div className='ReviewForm'>
+      {props.auth.isAuthenticated
+        ? [<div className="row" key='ReviewForm-textarea'>
+            <div className="col-sm-12">
+              <div className="form-group">
+                <lable className='contorl-label' htmlFor='ReviewForm-textarea'>
 
-      <div className="row">
-        <div className="col-sm-12">
-          <div className="form-group">
-            <lable className='contorl-label' htmlFor='ReviewForm-textarea'>
+                </lable>
+                <textarea
+                  placeholder='write your review'
+                  className='form-control'
+                  name="ReviewForm-textarea"
+                  id="ReviewForm-textarea"
+                  cols="30"
+                  rows="4">
+                </textarea>
+                <span className='ReviewForm-error'></span>
+              </div>
+            </div>
+          </div>,
 
-            </lable>
-            <textarea
-              placeholder='write your review'
-              className='form-control'
-              name="ReviewForm-textarea"
-              id="ReviewForm-textarea"
-              cols="30"
-              rows="4">
-            </textarea>
-          </div>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-sm-12 ">
-          <div
-            className='btn btn-primary pull-right'
-            onClick={e => handleSubmitComment(e)}
-          >
-            Submit
-          </div>
-        </div>
-      </div>
+          <div className="row" key='ReviewForm-submit'>
+            <div className="col-sm-12 ">
+              <div
+                className='btn btn-primary pull-right'
+                onClick={e => handleSubmitComment(e)}
+              >
+                Submit
+              </div>
+            </div>
+          </div>]
+        : null
+      }
 
     </div>
   )
