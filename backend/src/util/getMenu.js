b4013ecc0ed4b86463cd163590f728107e4bc76d
kk <kk@kk.PK5001Z>
2017-04-20 10:42:48 -0700

    adf

--- backend/src/util/getMenu.js
+++ backend/src/util/getMenu.js
@@ -33,12 +33,12 @@ export const getMenu = () => {
                                       as: "category.subcategory",
                                     },
                         },
-                        { // comment this block out would unfilter these two section in the query
-                          $project: {
-                            "category.subCategoryId": 0,
-                            "category.departmentId": 0,
-                          }
-                        },
+                        // { // comment this block out would unfilter these two section in the query
+                        //   $project: {
+                        //     "category.subCategoryId": 0,
+                        //     "category.departmentId": 0,
+                        //   }
+                        // },
                         {
                           $group: {
                             _id: "$_id",
