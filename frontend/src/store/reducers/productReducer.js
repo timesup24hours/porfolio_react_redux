4cfbc59916860c7506b8f24ca47f3d128dbcb6df
kk <kk@kk.PK5001Z>
2017-04-21 08:13:33 -0700

    change requesting reducer

--- frontend/src/store/reducers/productReducer.js
+++ frontend/src/store/reducers/productReducer.js
@@ -29,10 +29,43 @@ export default (state = initialState, action = {}) => {
     case actionTypes.PRODUCT_ADD_PRODUCT_REQUEST:
     case actionTypes.PRODUCT_SET_CURRENT_CATEGORY_PRODUCT_REQUEST:
     case actionTypes.PRODUCT_GET_CURRENT_CATEGORY_PRODUCT_REQUEST:
-    case actionTypes.PRODUCT_GET_CURRENT_PRODUCT_REVIEW_REQUEST:
+      return {
+        ...state,
+        pending: true,
+        success: false,
+        fail: false,
+        error: false,
+        errors: {},
+      }
     case actionTypes.PRODUCT_EDIT_PRODUCT_REQUEST:
+      return {
+        ...state,
+        currentEditProduct: null,
+        pending: true,
+        success: false,
+        fail: false,
+        error: false,
+        errors: {},
+      }
     case actionTypes.PRODUCT_DELETE_PRODUCT_REQUEST:
+      return {
+        ...state,
+        pending: true,
+        success: false,
+        fail: false,
+        error: false,
+        errors: {},
+      }
     case actionTypes.PRODUCT_GET_CURRENT_EDIT_PRODUCT_BY_OWNER_REQUEST:
+      return {
+        ...state,
+        currentEditProduct: null,
+        pending: true,
+        success: false,
+        fail: false,
+        error: false,
+        errors: {},
+      }
     case actionTypes.PRODUCT_GET_ONE_REQUEST:
       return {
         ...state,
